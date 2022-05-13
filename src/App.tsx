690276503b7f65a672d73534ce75d27e12213896
Petherson Ferraz <contato@pethersonferraz.com.br>
2022-05-12 21:56:24 -0300

    Envio de um arquivo que faltou salvar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import { useEffect, useState } from 'react';
-
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
 import { Content } from './components/Content';
 import { SideBar } from './components/SideBar';
 import { GenreModel } from './models/GenreModel';
 
-
-
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
   const [genres, setGenres] = useState<GenreModel[]>([]);
-
-  
   const [selectedGenre, setSelectedGenre] = useState<GenreModel>({} as GenreModel);
 
   useEffect(() => {
